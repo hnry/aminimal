8ab48609e46e0fab13f3912d17eaa5d5ab8618cd
hc <henycr@gmail.com>
2016-04-28 16:03:15 -0400

    fix side menu

--- TODO.md
+++ TODO.md
@@ -1,11 +1,5 @@
-- clear mobile menu by clicking page body (well  a wrapper that covers the body)
-- add swipe support for mobile menu (have to re-do the entire menu thing then as it'll act weird with the current title hack)
-
-- fix mobile after changes to full site
-- redo mobile menu
-
-- meta description and keyword
-- css transition for links
+- add swipe support for mobile menu
+- no side menu for widths < 280px~
 - sub-menus
 - social links
 - disqus support
--- static/js/aminimal.js
+++ static/js/aminimal.js
@@ -1,14 +1,17 @@
 var side = document.getElementById("side");
-var sidebox = document.getElementById("side-box-content");
+var license = document.getElementById("license");
 var open = document.getElementById("open-side");
-var container = document.getElementById("container");
 
 function closeMenu() {
+  open.innerHTML = ">";
+  license.style.display = "none";
   side.style.width = "0%";
 }
 open.addEventListener("click", function(e) {
   e.preventDefault();
   if (side.style.width === "0%" || side.style.width === "") {
+    open.innerHTML = "<";
+    license.style.display = "block";
     side.style.width = "60%";
   } else {
     closeMenu();
